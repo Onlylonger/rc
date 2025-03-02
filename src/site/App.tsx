97c11ad2e97657dc813e978e8e07ad0e59c56375
onlylonger <onlylonger@outlook.com>
2025-03-02 17:05:20 +0800

    U

--- src/site/App.tsx
+++ src/site/App.tsx
@@ -1,3 +1,4 @@
+import { ChevronRight, Loader2, MailOpen } from "lucide-react";
 import "./App.css";
 import {
   Button,
@@ -37,9 +38,27 @@ export function AlertDialogDemo() {
 
 function App() {
   return (
-    <div className="Root">
+    <div className="Root ">
       <Button>nihao</Button>
-      <AlertDialogDemo />
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+      <Button variant="outline" size="icon">
+        <ChevronRight />
+      </Button>
+      <Button>
+        <MailOpen /> Login with Email
+      </Button>
+      <Button disabled>
+        <Loader2 className="animate-spin" />
+        Please wait
+      </Button>
+      <Button asChild>
+        <a href="/login">Login</a>
+      </Button>
+      {/* <AlertDialogDemo /> */}
     </div>
   );
 }
