70e76e14a0ed1ccb059b69a9311a46a59aeae002
onlylonger <onlylonger@outlook.com>
2025-03-02 18:14:27 +0800

    chore: update

--- packages/rc/vite.config.ts
+++ packages/rc/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     dts({
       outDir: resolve(__dirname, "dist/es"),
-      tsconfigPath: resolve(__dirname, "./tsconfig.lib.json"),
+      tsconfigPath: resolve(__dirname, "./tsconfig.json"),
     }),
     // https://github.com/vitejs/vite/issues/15012#issuecomment-1825035992
     // https://github.com/evanw/esbuild/issues/3548
@@ -21,7 +21,7 @@ export default defineConfig({
     minify: false,
     cssMinify: false,
     rollupOptions: {
-      input: resolve(__dirname, "./src/shared/main.ts"),
+      input: resolve(__dirname, "./src/main.ts"),
       external: [...Object.keys(dependencies), "react/jsx-runtime"],
       output: [
         {
--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -8,10 +7,5 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "output",
-  },
-  resolve: {
-    alias: {
-      "@shilong/rc": resolve(__dirname, "./src/shared/main.ts"),
-    },
   },
 });
